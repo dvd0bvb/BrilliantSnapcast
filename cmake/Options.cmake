396a374a608ea5c20173ab50086d442e119f2f6f
David Brill <6david6brill6@gmail.com>
2025-10-20 17:48:04 -0400

    Updates to build/CI from BrilliantCMake

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -3,7 +3,7 @@ option(BRILLIANT_CMAKE_BUILD_EXECUTABLE "Build the project as an executable"
 )
 option(BRILLIANT_CMAKE_BUILD_SHARED "Build project as a shared library" OFF)
 option(BRILLIANT_CMAKE_BUILD_HEADER_ONLY
-       "Build project as a header only library" ON
+       "Build project as a header only library" OFF
 )
 option(BRILLIANT_CMAKE_CODE_COVERAGE "Build project with code coverage" OFF)
 
@@ -13,11 +13,16 @@ set(BRILLIANT_CMAKE_SANITIZER
 )
 set_property(CACHE BRILLIANT_CMAKE_SANITIZER PROPERTY STRINGS ASAN TSAN MSAN)
 
-set(BRILLIANT_CMAKE_MSAN_LIBCXX_INCLUDE_DIR
+option(BRILLIANT_CMAKE_LINK_LIBCXX "Link project to libc++" OFF)
+set(BRILLIANT_CMAKE_LIBCXX_INCLUDE_DIR
     ""
     CACHE STRING "Include path to the MSAN std lib"
 )
-set(BRILLIANT_CMAKE_MSAN_LIBCXX_LINK_DIR
+set(BRILLIANT_CMAKE_LIBCXX_LINK_DIR
     ""
     CACHE STRING "Link path to the MSAN std lib"
+)
+
+option(BRILLIANT_CMAKE_LINK_STATIC_RUNTIME "Statically link the c++ runtime"
+       OFF
 )
